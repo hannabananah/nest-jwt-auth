bf6da31cb2fc743c9570692f1c935fead478e676
HannahKang <hanna0175@gmail.com>
2024-06-12 12:37:05 +0900

    🏷️ chore : 텍스트 수정 및 타입 정의

--- src/auth/strategies/at.strategy.ts
+++ src/auth/strategies/at.strategy.ts
@@ -1,17 +1,18 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { Injectable } from '@nestjs/common';
+import { JwtPayload } from '../types';
 
 @Injectable()
 export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'at_secret',
+      secretOrKey: 'at-secret',
     });
   }
 
-  validate(payload: any) {
+  validate(payload: JwtPayload) {
     return payload;
   }
 }
