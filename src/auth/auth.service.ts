e92a11663f754979f76bae89074c00644652adc8
HannahKang <hanna0175@gmail.com>
2024-06-12 09:44:28 +0900

    ✨ feat : Auth DTO 및 인증 서비스 구현 중간커밋
    - AuthDto: 데코레이터를 통한 유효성 검사
    - bcrypt 라이브러리를 사용한 비밀번호 해싱
    - JWT 토큰 생성 메서드 추가
    - 토큰생성 구현, 신규가입 구현중

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from './../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as bcrypt from 'bcrypt';
+import { Tokens } from './types';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  hashData(data: string) {
+    return bcrypt.hash(data, 10);
+  }
+
+  async getTokens(userId: number, email: string): Promise<Tokens> {
+    const jwtPayload = {
+      sub: userId,
+      email: email,
+    };
+
+    const [at, rt] = await Promise.all([
+      this.jwtService.signAsync(jwtPayload, {
+        secret: 'at_secret',
+        expiresIn: 60 * 15,
+      }),
+      this.jwtService.signAsync(jwtPayload, {
+        secret: 'rt_secret',
+        expiresIn: 60 * 60 * 24 * 7,
+      }),
+    ]);
+
+    return {
+      access_token: at,
+      refresh_token: rt,
+    };
+  }
+
+  async signupLocal(dto: AuthDto): Promise<Tokens> {
+    const hash = await this.hashData(dto.password);
+    const newUser = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+
+    const tokens = await this.getTokens(newUser.id, newUser.email);
+
+    return tokens;
+  }
+  signinLocal() {}
+  logout() {}
+  refreshTokens() {}
+}
